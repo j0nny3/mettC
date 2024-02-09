0d4c814b5368e8e8e2c08bee745532a6fd0d18fc
jonas rumstedt <Jonas.Rumstedt@msg-david.de>
2024-02-09 13:04:24 +0100

    Remove logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CalculatorComponent} from "./calculator/calculator.component";
+import {DarkModeService} from "angular-dark-mode";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mettCalc';
+  logo: any;
+
+
+
+  // onToggleDark() {
+  //   console.log("toggled")
+  //   this.darkModeService.toggle();
+  // }
 }
